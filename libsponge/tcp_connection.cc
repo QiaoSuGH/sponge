291546f35d44fe4a43e72ee75b8192b4f470a1e7
QiaoSuGH <284250386@qq.com>
2021-03-17 08:05:57 -0700

    a few bugs existing

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -22,7 +22,7 @@ size_t TCPConnection::time_since_last_segment_received() const { return _time_si
 
 void TCPConnection::clean_shutdown(){
     if(_receiver.stream_out().input_ended()){
-        if(_sender.stream_in().input_ended()){
+        if(!_sender.stream_in().input_ended()){
             _linger_after_streams_finish = false;
         }
         else{
@@ -55,18 +55,19 @@ void TCPConnection::unclean_shutdown(){
 
 
 void TCPConnection::segment_received(const TCPSegment &seg) { 
-    /*
     if(!_active)return;
-    if(seg.header().rst){
-        unclean_shutdown();
-        return;
-    }
 
     _time_since_last_seg_received = 0;
     
     //according to diff state to process
     //the sender is closed and the receiver is listening
     if(_receiver.ackno().has_value() == false && _sender.next_seqno_absolute() == 0){
+        if(seg.header().rst){
+            _sender.stream_in().set_error();
+            _receiver.stream_out().set_error();
+            _active = false;
+            return;
+        }
         //haven't got a SYN
         if(seg.header().syn == false)return;
         _receiver.segment_received(seg);
@@ -78,88 +79,44 @@ void TCPConnection::segment_received(const TCPSegment &seg) {
     if(_sender.next_seqno_absolute() > 0 && _receiver.ackno().has_value() == false &&
        _sender.bytes_in_flight() == _sender.next_seqno_absolute())
     {
+        if(seg.header().rst){
+            _sender.stream_in().set_error();
+            _receiver.stream_out().set_error();
+            _active = false;
+            return;
+        }
         //try to establish connection with endpoints
         if(seg.payload().size() > 0)return;//SYN should not carry data
-        */
-        /*if(seg.header().ack == false){
-            //possible when the endpoints try to establish conn at the same time
-            if(seg.header().syn){
-                _receiver.segment_received(seg);
-                _sender.send_empty_segment();//
-            }
-        }
-        else{
-            _receiver.segment_received(seg);
-            _sender.ack_received(seg.header().ackno, seg.header().win);
-        }*/
         //expect a SYN may be without a ack
-    /*
         if(seg.header().syn == false)return;
         if(seg.header().ack){
             _receiver.segment_received(seg);
             _sender.ack_received(seg.header().ackno, seg.header().win);
+            if(_sender.stream_in().buffer_empty()==true)
+            _sender.send_empty_segment();
+            send_segs_in_sender();
         }
         else{
+            //possible when simultaneous open
             _receiver.segment_received(seg);
             _sender.send_empty_segment();//ack for SYN
         }
+        clean_shutdown();
         return ;
     }
 
-    //SYN acked
-    //if()
-
-    //established
-    _receiver.segment_received(seg);
-    _sender.ack_received(seg.header().ackno, seg.header().win);
-    if (_sender.stream_in().buffer_empty() && seg.length_in_sequence_space())
-        _sender.send_empty_segment();
-
-    send_segs_in_sender();
-    */
-   if (!_active)
-        return;
-    _time_since_last_seg_received = 0;
-    // State: closed
-    if (!_receiver.ackno().has_value() && _sender.next_seqno_absolute() == 0) {
-        if (!seg.header().syn)
-            return;
-        _receiver.segment_received(seg);
-        connect();
-        return;
-    }
-    // State: syn sent
-    if (_sender.next_seqno_absolute() > 0 && _sender.bytes_in_flight() == _sender.next_seqno_absolute() &&
-        !_receiver.ackno().has_value()) {
-        if (seg.payload().size())
-            return;
-        if (!seg.header().ack) {
-            if (seg.header().syn) {
-                // simultaneous open
-                _receiver.segment_received(seg);
-                _sender.send_empty_segment();
-            }
-            return;
-        }
-        if (seg.header().rst) {
-            _receiver.stream_out().set_error();
-            _sender.stream_in().set_error();
-            _active = false;
-            return;
-        }
-    }
-    _receiver.segment_received(seg);
-    _sender.ack_received(seg.header().ackno, seg.header().win);
-    // Lab3 behavior: fill_window() will directly return without sending any segment.
-    // See tcp_sender.cc line 42
-    if (_sender.stream_in().buffer_empty() && seg.length_in_sequence_space())
-        _sender.send_empty_segment();
-    if (seg.header().rst) {
-        _sender.send_empty_segment();
+    if(seg.header().rst){
         unclean_shutdown();
         return;
     }
+    _receiver.segment_received(seg);
+    _sender.ack_received(seg.header().ackno, seg.header().win);
+
+    if (_sender.stream_in().buffer_empty() && seg.length_in_sequence_space())
+        _sender.send_empty_segment();
+
     send_segs_in_sender();
+    clean_shutdown();
 }
 
 bool TCPConnection::active() const { return _active; }
@@ -188,6 +145,7 @@ size_t TCPConnection::write(const string &data) {
     _sender.fill_window();
 
     send_segs_in_sender();
+    clean_shutdown();
     return num_written_bytes;
 
 }
@@ -200,17 +158,20 @@ void TCPConnection::tick(const size_t ms_since_last_tick) {
     if (_sender.consecutive_retransmissions() > TCPConfig::MAX_RETX_ATTEMPTS)
         unclean_shutdown();
     send_segs_in_sender();
+    clean_shutdown();
 }
 
 void TCPConnection::end_input_stream() {
     _sender.stream_in().end_input();
     _sender.fill_window();
     send_segs_in_sender();
+    clean_shutdown();
 }
 
 void TCPConnection::connect() {
     _sender.fill_window();
     send_segs_in_sender();
+    clean_shutdown();
 }
 
 TCPConnection::~TCPConnection() {
